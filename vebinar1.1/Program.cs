cf53cc2086e5c4e7fd18baf7a379a8af007c3f87
sam <sam.131313@mail.ru>
2022-11-24 14:56:40 +0300

    задача 2

---
+++ vebinar1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого.
+a = 5; b = 25 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> нет
+a = -3 b = 9 -> да
+*/
+
+int userNumber1 = new int();
+Console.WriteLine("Введите число");
+int userNumber2 = new int();
+Console.WriteLine("Введите число");
+userNumber1 = Convert.ToInt32(Console.ReadLine());
+int result = userNumber1 * userNumber1;
+Console.WriteLine(result);
