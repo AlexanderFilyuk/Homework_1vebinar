f035e4ae93d9220619f52361d801aeaf33074adb
sam <sam.131313@mail.ru>
2022-11-29 02:23:59 +0300

    решение

---
+++ homework1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* напишите программу, которая на вход принимает число и показывает является ли оно четным*/
+
+
+int userNumber1 = new int();
+Console.WriteLine("Введите число");
+    userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber1 % 2;
+if  (result == 0)
+    Console.WriteLine("Четное");
+else    
+{Console.WriteLine("Нечетное");}
+
+
