975c31fe8c2a9aa7420d99f7890ef9eb507a4b22
sam <sam.131313@mail.ru>
2022-11-29 14:38:14 +0300

    решение

---
+++ Homework1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая на вход принимает (N), а на выходе показывает все четные числа от 1 до N */
+
+int N = new int();
+Console.WriteLine("Введите число");
+N = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+Console.WriteLine("Четные числа");
+
+    while (result < N - 1)
+    {
+        result +=2;
+        Console.WriteLine(result);
+    }   
+
+
+
