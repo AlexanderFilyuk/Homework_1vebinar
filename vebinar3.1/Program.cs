f242cedea03ea03f759ea43c4918e93e1189053e
sam <sam.131313@mail.ru>
2022-12-04 07:09:42 +0300

    ыторая задача недоделана

--- vebinar3.1/Program.cs
+++ vebinar3.1/Program.cs
@@ -3,7 +3,7 @@
 //четверти (x и y).
 
 
-
+/*
 
 int GetCoordinateFromQuarter(int quarter)
 {
@@ -35,10 +35,34 @@ int GetNumberFromUser(string message)
 int quarter = GetNumberFromUser("Введите четветь: ");
 GetCoordinateFromQuarter(quarter);
 
+*/
 
 
+//Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
 
 
+int GetNumberFromUser(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+Double GetDistance (int x1, int y1, int x2, int y2)
+{
+    double distance = Match.Sqrt(Match.Pow (x2 - x1,2) + Match.Pow(y2 - y1,2));
+    return double;
+}
+int x1 = GetNumberFromUser("Введите x1: ");
+int y1 = GetNumberFromUser("Введите y1: ");
+int x2 = GetNumberFromUser("Введите x2: ");
+int y2 = GetNumberFromUser("Введите y2: ");
+int distance = GetDistance (int x1, int y1, int x2, int y2);
+Console.WriteLine($"Расстояние между точками = {distance} ");
 
 
 
