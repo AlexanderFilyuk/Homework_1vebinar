a7f2c35ee7c23c11292d4d14fe395e7e2e812760
sam <sam.131313@mail.ru>
2022-12-04 06:01:46 +0300

    решение

---
+++ vebinar3.1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой 
+//четверти (x и y).
+
+
+
+
+int GetCoordinateFromQuarter(int quarter)
+{
+    if (quarter == 1)
+    {
+        Console.WriteLine("x > 0 и y > 0");
+    }
+    else if (quarter == 2)
+    {
+        Console.WriteLine("x < 0 и y > 0");
+    }
+    else if (quarter == 3)
+    {
+        Console.WriteLine("x < 0 и y < 0");
+    }
+    else if (quarter == 4)
+    {
+        Console.WriteLine("x > 0 и y < 0");
+    }
+    return quarter;
+}
+int GetNumberFromUser(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int quarter = GetNumberFromUser("Введите четветь: ");
+GetCoordinateFromQuarter(quarter);
+
+
+
+
+
+
+
+
+
