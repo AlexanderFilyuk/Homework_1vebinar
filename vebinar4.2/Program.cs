d474260ea830073e8f2d5f729692c55e5ada91e0
sam <sam.131313@mail.ru>
2022-12-13 17:11:56 +0300

    2 задачи решение

---
+++ vebinar4.2/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт 
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+/*
+
+Console.Write("Введите число ");
+string writen = Console.ReadLine();
+int N = Convert.ToInt32(writen);
+
+int GetFactorial(int N)
+{
+    int i = 1;
+    for (int num = 1; num <= N; num++)
+    {
+        i = i * num;
+    }
+    return i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} = {GetFactorial(N)}");
+Console.WriteLine();
+
+*/
+
+
+/*Напишите программу, которая выводит массив из 8 элементов,
+ заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+
+int N = 8;
+int min = 0;
+int max = 1;
+
+int [] CreatArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr [i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreatArray(N, min, max);
+PrintArray(array);
+
+
