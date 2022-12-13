f77f82a8e3c34aac9e615ebe8e0d83b42a736e32
sam <sam.131313@mail.ru>
2022-12-13 18:25:54 +0300

    решение

---
+++ Homework4/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int N = 8;
+int min = 0;
+int max = 100;
+
+int [] CreatArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr [i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreatArray(N, min, max);
+PrintArray(array);
+
+
+
