c41e2a83c60686adbef3dfd4263c13fb0b536d08
sam <sam.131313@mail.ru>
2022-12-13 12:54:15 +0300

    решение

---
+++ vebinar4/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+
+
+int sumofRange(int start, int finish)
+{
+    int sum = 0;
+    for (int i = start; i <= finish; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+int getNumberFromUser(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result; 
+}
+
+int userNumber = getNumberFromUser("Введите А для подсчета суммы 1...А");
+int sum = SumofRange(1, userNumber);
+Console.WriteLine($"Сумма чисел от 1..{userNumber} = {sum}");
+
+
