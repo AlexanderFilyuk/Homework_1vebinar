3324160244d8ba3cd3158e1074e2979c7e541e57
sam <sam.131313@mail.ru>
2022-12-18 11:41:14 +0300

    решение

---
+++ vebinar6.2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*  Напишите программу, которая будет преобразовывать
+ десятичное число в двоичное.
+
+45 -> 101101
+
+3 -> 11
+
+2 -> 10
+*/
+
+
+int inputUserNumber(string userMessage)
+{
+    Console.WriteLine(userMessage);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+string transformationNumber(int number)
+{
+    int buf = 0;
+    string result = "";
+    while(number > 0)
+    {
+        buf = number % 2;
+        result = buf + result;
+        number /= 2; 
+    }
+    return result;
+}
+int userNumber = inputUserNumber("Введите число ");
+string binaryNumber = transformationNumber(userNumber);
+Console.WriteLine(binaryNumber);
+
+
+
+
